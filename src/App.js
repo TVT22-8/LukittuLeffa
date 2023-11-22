1c36ab20df606b3482cf192b3ea83e5f29d927bf
EliasPiekkola <112543659+EliasPiekkola@users.noreply.github.com>
2023-11-22 16:54:32 +0200

    Frontendiä/router
    Frontendiä tehty, routeja hieman.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+// Filename: App.js
 import './App.css';
+import Navbar from './components/Navbar';
+import { SearchBar } from './components/SearchBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/pages/settings.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const settings = () => {
+  return <div>settings</div>
+  
+}
