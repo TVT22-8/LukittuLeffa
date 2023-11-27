42723e63c9836931094fb12d9050d0062ec9a54c
Sakkeyy <sukarid@hotmail.com>
2023-11-27 13:20:37 +0200

    Testing merge compatibility
    minimal changes

--- lukittunode/src/main.js
+++ lukittunode/src/main.js
@@ -4,7 +4,7 @@ const userRoutes = require('./routes/dbRoutes');//Include every route created la
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());//For parsing JSON data
-app.use('/api', userRoutes);//For future routes also copy paste modify this
+
 
 console.log('Main logic running');
 
