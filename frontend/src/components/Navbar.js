e13b229d8fe5a46ea21ebb3c2ac9047ad713862a
EliasPiekkola <112543659+EliasPiekkola@users.noreply.github.com>
2023-11-29 15:16:25 +0200

    frontendiä

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,76 @@
-// Navbar.js
-import React from 'react';
-import './Navbar.css';
+import React, { useState } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import { FaSun, FaMoon } from 'react-icons/fa';
-const Navbar = () => {
-  //const { isDarkMode, toggleDarkMode } = useTheme();
+import './Navbar.css';
+import Dropdown from './Dropdown';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
 
   return (
-    <nav>
-      <Link to="/"> <h1>Lukittuleffa</h1> </Link>
-      <ul>
-        <li>
-          <Link to="/my_groups"> My groups </Link>
-        </li>
-        <li>
-          <Link to="/settings">settings </Link>
-        </li>
-        <li>
-          <Link to="/groups"> groups</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          EPIC
+          <i class='fab fa-firstdraft' />
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li
+            className='nav-item'
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to='/services'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Services <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li>
+            <Link
+              to='/sign-up'
+              className='nav-links-mobile'
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        <Button />
+      </nav>
+    </>
   );
-};
+}
 
 export default Navbar;
