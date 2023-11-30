134e1072e73fee8dc33e8b9fe3a0253ebd6608b1
Mikcsu <miika.puominen@gmail.com>
2023-11-30 13:40:16 +0200

    vähä kaikkea

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Testfetch from './jsxfiles/Testfetch';
 
 const Home = () => {
   return (
     <div>
-      
+      <Testfetch />
       <h2>Welcome to the Home Page!</h2>
     </div>
   );
