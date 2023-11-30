622a19c17565314bf7a9ca653a7198b73a6268f7
EliasPiekkola <112543659+EliasPiekkola@users.noreply.github.com>
2023-11-30 15:48:16 +0200

    frontendiä

--- frontend/src/components/pages/Group.js
+++ frontend/src/components/pages/Group.js
@@ -1,12 +1,18 @@
+// Group.js
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import './GroupList.css'; // Import your CSS file
 
 const Group = () => {
+  const { id } = useParams();
+  const group = { id, name: `Group ${id}`, description: 'Group description' };
+
   return (
-    <div>
-      <h2>Contact Us</h2>
-      <p>You can reach us at example@email.com.</p>
+    <div className="group-detail-container">
+      <h2>{group.name}</h2>
+      <p>{group.description}</p>
     </div>
   );
-}
+};
 
 export default Group;
