e31072a762cd3a188d009f1dda98c508283512e0
EliasPiekkola <112543659+EliasPiekkola@users.noreply.github.com>
2023-12-05 14:31:43 +0200

    frontendiä

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,11 +1,27 @@
-import React from 'react';
+// Button.js
+import React, { useState } from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
+import AuthPopup from './AuthPopup'; // Create AuthPopup component in a separate file
 
 export function Button() {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const handleOpenPopup = () => {
+    setPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setPopupOpen(false);
+  };
+
   return (
-    <Link to='sign-up'>
-      <button className='btn'>Sign Up</button>
-    </Link>
+    <div>
+      <Link to='#' onClick={handleOpenPopup}>
+        <button className='btn'>Sign Up</button>
+      </Link>
+
+      {isPopupOpen && <AuthPopup onClose={handleClosePopup} />}
+    </div>
   );
 }
--- frontend/src/components/pages/Group.js
+++ frontend/src/components/pages/Group.js
@@ -1,16 +1,38 @@
 // Group.js
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import './GroupList.css'; // Import your CSS file
+import React, { useState, useEffect } from 'react';
+import GroupItem from '../GroupItem';
 
 const Group = () => {
-  const { id } = useParams();
-  const group = { id, name: `Group ${id}`, description: 'Group description' };
+  const [groups, setGroups] = useState([]); // Fetch groups from your database
+
+  // Example: Fetch groups from your database
+  useEffect(() => {
+    // Your API call or database query to get groups
+    // Update the groups state with the result
+    // For example:
+    // const fetchedGroups = fetchGroupsFromDatabase();
+    // setGroups(fetchedGroups);
+  }, []);
 
   return (
-    <div className="group-detail-container">
-      <h2>{group.name}</h2>
-      <p>{group.description}</p>
+    <div className="group-page">
+      <div className="in-group-section">
+        <h2>Groups You Are In</h2>
+        <div className="group-list">
+          {groups.map(group => (
+            <GroupItem key={group.id} group={group} isInGroup={true} />
+          ))}
+        </div>
+      </div>
+
+      <div className="not-in-group-section">
+        <h2>Groups You Are Not In</h2>
+        <div className="group-list">
+          {groups.map(group => (
+            <GroupItem key={group.id} group={group} isInGroup={false} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
