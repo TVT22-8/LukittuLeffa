0cacd137598a0a62a13aa4a40dcf99856cb9b7ef
Mikcsu <miika.puominen@gmail.com>
2023-12-07 15:01:10 +0200

    jtn

--- frontend/src/components/pages/jsxfiles/Testfetch.jsx
+++ frontend/src/components/pages/jsxfiles/Testfetch.jsx
@@ -42,81 +42,81 @@ const Testfetch = () => {
         <input type="text" value={movieId} onChange={(e) => setMovieId(e.target.value)} />
       </label>
       <button onClick={click}>Fetch Movie Information</button>
-      
-    <div style={{ display: 'flex', justifyContent: 'flex-start', margin: '0 100px' }}>
-      {movieInfo && (
-        <Card style={{ width: '25rem' }}>
-          <Card.Img variant="top" src={url + movieInfo.poster_path} /> {/* Corrected: Card.Img */}
-          <Card.Body>
-            <Card.Title>Title: {movieInfo.title}</Card.Title>
-            <Card.Text>
-              Release date: {movieInfo.release_date}
+
+      <div style={{ display: 'flex', justifyContent: 'flex-start', margin: '0 100px' }}>
+        {movieInfo && (
+          <Card style={{ width: '25rem' }}>
+            <Card.Img variant="top" src={url + movieInfo.poster_path} /> {/* Corrected: Card.Img */}
+            <Card.Body>
+              <Card.Title>Title: {movieInfo.title}</Card.Title>
+              <Card.Text>
+                Release date: {movieInfo.release_date}
               </Card.Text>
               <Card.Text>
-              Duration: {movieInfo.runtime} Minutes
+                Duration: {movieInfo.runtime} Minutes
               </Card.Text>
-            <Button variant="primary">Add to ...</Button>
-          </Card.Body>
-        </Card>
-      )}
+              <Button variant="primary">Add to ...</Button>
+            </Card.Body>
+          </Card>
+        )}
       </div>
-      <div style={{position: 'absolute', top: '100px', left: '500px' , display: 'flex', justifyContent: 'flex-end', margin: '0 50px' }}>
-      {movieInfo && (
-        <Card style={{ width: '700px', height: '300px' }}>
-          <Card.Body>
-          <Card.Title style={{textAlign: 'center', fontSize: '3rem'}}>{movieInfo.title}</Card.Title>
-            <Card.Text style={{fontSize: '1.1rem'}}>
-              <strong>Description:</strong> {movieInfo.overview}
+      <div style={{ position: 'absolute', top: '100px', left: '500px', display: 'flex', justifyContent: 'flex-end', margin: '0 50px' }}>
+        {movieInfo && (
+          <Card style={{ width: '700px', height: '300px' }}>
+            <Card.Body>
+              <Card.Title style={{ textAlign: 'center', fontSize: '3rem' }}>{movieInfo.title}</Card.Title>
+              <Card.Text style={{ fontSize: '1.1rem' }}>
+                <strong>Description:</strong> {movieInfo.overview}
               </Card.Text>
-          </Card.Body>
-        </Card>
-      )}
+            </Card.Body>
+          </Card>
+        )}
       </div>
-      <div style={{position: 'absolute',top: '100px',right: '350px' , display: 'flex-end',flexDirection: 'row', justifyContent: 'space-between', margin: '0 0px' }}>
-      {castInfo &&(
-        <Card style={{ maxHeight: '800px',width: "300px", overflowY: 'auto'  }}>
-          <Card.Body>
-            <Card.Title>Cast:</Card.Title>
-            <Card.Text>
-            {castInfo.cast.map((actor, index) => (
-                      <span key={actor.id} style={{ display: 'flex', alignItems: 'center' }}>
-                        {index > 0 && <br />}
-                        <Card.Img style={{height:"20%", width:"20%", marginRight: '10px'}} src={url2 + actor.profile_path} />
-                        <div>
-                  <div>{actor.name}</div>
-                  <div>{actor.character}</div>
-          </div>
-                      </span>
-                    ))}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      )}
+      <div style={{ position: 'absolute', top: '100px', right: '350px', display: 'flex-end', flexDirection: 'row', justifyContent: 'space-between', margin: '0 0px' }}>
+        {castInfo && (
+          <Card style={{ maxHeight: '800px', width: "300px", overflowY: 'auto' }}>
+            <Card.Body>
+              <Card.Title>Cast:</Card.Title>
+              <Card.Text>
+                {castInfo.cast.map((actor, index) => (
+                  <span key={actor.id} style={{ display: 'flex', alignItems: 'center' }}>
+                    {index > 0 && <br />}
+                    <Card.Img style={{ height: "20%", width: "20%", marginRight: '10px' }} src={url2 + actor.profile_path} />
+                    <div>
+                      <div>{actor.name}</div>
+                      <div>{actor.character}</div>
+                    </div>
+                  </span>
+                ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
       </div>
-      <div style={{position: 'absolute',top: '100px',right: '20px' , display: 'flex-end',flexDirection: 'row', justifyContent: 'space-between', margin: '0 0px' }}>
-{castInfo &&(
-        <Card style={{ maxHeight: '800px',width: "300px", overflowY: 'auto'  }}>
-          <Card.Body>
-            <Card.Title>Workers:</Card.Title>
-            <Card.Text>
-            {castInfo.directing.map((actor, index) => (
-                      <span key={actor.id} style={{ display: 'flex', alignItems: 'center' }}>
-                        {index > 0 && <br />}
-                        <Card.Img style={{height:"20%", width:"20%", marginRight: '10px'}} src={url2 + actor.profile_path} />
-                        <div>
-                  <div>{actor.name}</div>
-                  <div>{actor.character}</div>
-          </div>
-                      </span>
-                    ))}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      )}
+      <div style={{ position: 'absolute', top: '100px', right: '20px', display: 'flex-end', flexDirection: 'row', justifyContent: 'space-between', margin: '0 0px' }}>
+        {castInfo && (
+          <Card style={{ maxHeight: '800px', width: "300px", overflowY: 'auto' }}>
+            <Card.Body>
+              <Card.Title>Workers:</Card.Title>
+              <Card.Text>
+                {castInfo.directing.map((actor, index) => (
+                  <span key={actor.id} style={{ display: 'flex', alignItems: 'center' }}>
+                    {index > 0 && <br />}
+                    <Card.Img style={{ height: "20%", width: "20%", marginRight: '10px' }} src={url2 + actor.profile_path} />
+                    <div>
+                      <div>{actor.name}</div>
+                      <div>{actor.character}</div>
+                    </div>
+                  </span>
+                ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
       </div>
-      
-      
-      </div>
+
+
+    </div>
   );
 };
 
