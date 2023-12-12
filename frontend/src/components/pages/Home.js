a73d53fab148ab6258cbd7f399489666d6d37b06
EliasPiekkola <112543659+EliasPiekkola@users.noreply.github.com>
2023-12-12 18:08:18 +0200

    Update Home.js

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Testfetch from './jsxfiles/Testfetch';
+import ShapeExample from './jsxComponents/ItemCard';
+import { useParams } from 'react-router-dom';
+
+
+const Home = () => {
+
+  
+  return (
+    <div>
+      <h3>Home Page</h3>
+
+    </div>
+  );
+}
+
+export default Home;
