b415ad7a0bff07cffa251afa08cb3df533b113b1
HannuOikarinen <oikarhan@gmail.com>
2023-12-13 20:12:34 +0200

    Finnkino_test lisätty

--- lukittunode/src/Tests/FinnkinoNews_test.js
+++ lukittunode/src/Tests/FinnkinoNews_test.js
@@ -1,27 +1,31 @@
 const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const { expect } = chai;
-const getSimilar = require('../utils/FinnkinoNews');
+const expect = chai.expect;
+const getFinnkinoNews = require('../utils/FinnkinoNews'); // Update the path as needed
 
-chai.use(chaiAsPromised);
+describe('Finnkino News API Tests', () => {
+  it('should fetch and parse Finnkino news successfully', async () => {
+    try {
+      const news = await getFinnkinoNews();
+      // Check if the result is an array
+      expect(news).to.be.an('array');
 
+      // Assuming at least one news article is returned
+      const firstArticle = news[0];
+      // Validate keys in the first article
+      expect(firstArticle).to.have.property('title');
+      expect(firstArticle).to.have.property('publishDate');
+      expect(firstArticle).to.have.property('htmlLead');
+      expect(firstArticle).to.have.property('articleURL');
+      expect(firstArticle).to.have.property('imageURL');
+      expect(firstArticle).to.have.property('thumbnailURL');
 
-// Mocking the fetch function for testing purposes
-const mockFetch = async (url) => {
-  if (url.includes('/news')) { 
-    const MockData = {
-
+      // Assuming date format for publishDate
+      expect(/^\d{4}-\d{2}-\d{2}/.test(firstArticle.publishDate)).to.be.true;
+    } catch (error) {
+      // Fail the test if an error occurs during execution
+      throw new Error('Test failed: ' + error.message);
     }
-    
-    return Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve(MockData),
-      });
-    } else {
-      return Promise.resolve({
-        ok: false,
-        status: 404,
-        statusText: 'Not Found'
-      });
-    }
-}
+  });
+
+
+});
