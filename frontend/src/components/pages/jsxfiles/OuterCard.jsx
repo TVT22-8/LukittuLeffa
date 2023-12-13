822ccc20b13011a7b140530f59cdfe683d7d2425
Mikcsu <miika.puominen@gmail.com>
2023-12-13 20:35:32 +0200

    home page kohta valamis

--- frontend/src/components/pages/jsxfiles/OuterCard.jsx
+++ frontend/src/components/pages/jsxfiles/OuterCard.jsx
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 
 const OuterCard = ({ children }) => {
   return (
-    <Card style={{ width: '1800px', height: '325px', overflowX: 'auto'}}>
+    <Card style={{ height: '325px', overflowX: 'auto'}}>
       <Card.Body>{children}</Card.Body>
     </Card>
   );
